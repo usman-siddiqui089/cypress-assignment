30d3f11544c9fda5c92ec562fd5a29f801df5fe8
UsmanSiddiqui <usmansiddiqui@A006-427.local>
2021-08-03 18:19:13 +0500

    Used generic timeouts

--- cypress/integration/assignment.spec.js
+++ cypress/integration/assignment.spec.js
@@ -5,43 +5,43 @@ describe('Digital Tolk Tests', () => {
 
     it('TS-01 Navigate to Home Page', () => {
         cy.visit('https://ct.digitaltolk.se/')
-        cy.contains('Logga in', {timeout: 10000}).eq(0).should('be.visible')
+        cy.contains('Logga in').eq(0).should('be.visible')
     })
 
     it('TS-02 Change Language', () => {
         cy.get('.language-menu').should('be.visible').click()
-        cy.contains('EN', {timeout: 5000}).should('be.visible').click()
-        cy.contains('Log in', {timeout: 10000}).eq(0).should('be.visible')
+        cy.contains('EN').should('be.visible').click()
+        cy.contains('Log in').eq(0).should('be.visible')
     })
 
     it('TS-03 Navigate to login page', () => {
-        cy.contains('Log in', {timeout: 10000}).eq(0).should('be.visible').click()
+        cy.contains('Log in').eq(0).should('be.visible').click()
         cy.url().should('include', '/login')
     })
 
     it('TS-04 Login with invalid credentials', () => {
-        cy.get('input[placeholder="Username"]', {timeout: 10000})
+        cy.get('input[placeholder="Username"]')
             .should('be.visible').type('username')
-        cy.get('input[placeholder="Password"]', {timeout: 10000})
+        cy.get('input[placeholder="Password"]')
             .should('be.visible').type('password')
         cy.get('.login-form__login-button').should('be.visible').click()
-        cy.get('div[role="alert"]', {timeout: 10000}).should('be.visible').and('contain', 'The user credentials were incorrect.')
+        cy.get('div[role="alert"]').should('be.visible').and('contain', 'The user credentials were incorrect.')
     })
 
     it('TS-05 Login with valid credentials', () => {
-        cy.get('input[placeholder="Username"]', {timeout: 10000})
+        cy.get('input[placeholder="Username"]')
             .should('be.visible').clear().type('usman@customer.dt')
-        cy.get('input[placeholder="Password"]', {timeout: 10000})
+        cy.get('input[placeholder="Password"]')
             .should('be.visible').clear().type('Test123$')
         cy.get('.login-form__login-button').should('be.visible').click()
-        cy.get('div[role="alert"]', {timeout: 10000}).should('not.be.visible')
+        cy.get('div[role="alert"]').should('not.be.visible')
         cy.url().should('not.contain', '/login')
     })
 
     it('TS-06 Navigate to Booking Tab', () => {
-        cy.get('input[placeholder="Select Language"]', {timeout: 10000})
+        cy.get('input[placeholder="Select Language"]')
             .should('be.visible').click().type('Ludh', {delay: 500}).type('{enter}')
-        cy.get('input[placeholder="Day"]', {timeout: 10000})
+        cy.get('input[placeholder="Day"]')
             .should('be.visible').click()
         cy.get(`span[aria-label="${dateTimeObj.day}"]`)
             .should('be.visible').click()
